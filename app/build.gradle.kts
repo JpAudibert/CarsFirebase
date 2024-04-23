3455e2b75747dc36701a45ba18f1825f475585c8
João Pedro Basso Audibert <hotsjoao@gmail.com>
2024-04-23 20:09:16 -0300

    adds firebase connection

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.jetbrainsKotlinAndroid)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -42,6 +43,9 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.firebase:firebase-database:20.3.1")
+    implementation("com.firebaseui:firebase-ui-database:8.0.0")
+    implementation("com.google.android.material:material:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
